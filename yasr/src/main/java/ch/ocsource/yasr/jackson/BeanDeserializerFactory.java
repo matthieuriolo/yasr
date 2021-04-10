4cd95342dc9af1967a5e35f912669f14b2cfd4f0
Matthieu Riolo <matthieu.riolo@googlemail.com>
2021-04-11 00:33:25 +0200

    override withConfig() in BeanDeserializerFactory to make jackson work with java 8 datetime

--- yasr/src/main/java/ch/ocsource/yasr/jackson/BeanDeserializerFactory.java
+++ yasr/src/main/java/ch/ocsource/yasr/jackson/BeanDeserializerFactory.java
@@ -5,6 +5,8 @@ import java.nio.file.Path;
 import com.fasterxml.jackson.databind.BeanDescription;
 import com.fasterxml.jackson.databind.DeserializationContext;
 import com.fasterxml.jackson.databind.cfg.DeserializerFactoryConfig;
+import com.fasterxml.jackson.databind.deser.DeserializerFactory;
+import com.fasterxml.jackson.databind.util.ClassUtil;
 
 public class BeanDeserializerFactory extends com.fasterxml.jackson.databind.deser.BeanDeserializerFactory {
 	private static final long serialVersionUID = -6989867428476804889L;
@@ -19,4 +21,19 @@ public class BeanDeserializerFactory extends com.fasterxml.jackson.databind.dese
             BeanDescription beanDesc) {
 		return new BeanDeserializerBuilder(path, beanDesc, ctxt);
     }
+	
+	@Override
+    public DeserializerFactory withConfig(DeserializerFactoryConfig config) {
+		if (_factoryConfig == config) {
+            return this;
+        }
+        /* 22-Nov-2010, tatu: Handling of subtypes is tricky if we do immutable-with-copy-ctor;
+         *    and we pretty much have to here either choose between losing subtype instance
+         *    when registering additional deserializers, or losing deserializers.
+         *    Instead, let's actually just throw an error if this method is called when subtype
+         *    has not properly overridden this method; this to indicate problem as soon as possible.
+         */
+        ClassUtil.verifyMustOverride(BeanDeserializerFactory.class, this, "withConfig");
+        return new BeanDeserializerFactory(path, config);
+	}
 }
