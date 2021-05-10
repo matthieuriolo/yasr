f256d11d421871ee716f21345005eb5b8db03740
Matthieu Riolo <matthieu.riolo@googlemail.com>
2021-05-10 23:41:40 +0200

    remove unnecessary imports

--- yasr/src/main/java/ch/ocsource/yasr/parser/version1_0_0/Converter.java
+++ yasr/src/main/java/ch/ocsource/yasr/parser/version1_0_0/Converter.java
@@ -2,17 +2,8 @@ package ch.ocsource.yasr.parser.version1_0_0;
 
 import java.io.File;
 import java.io.IOException;
-import java.nio.file.Paths;
-
 import com.fasterxml.jackson.core.JsonParseException;
 import com.fasterxml.jackson.databind.JsonMappingException;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.databind.cfg.DeserializerFactoryConfig;
-import com.fasterxml.jackson.databind.deser.DefaultDeserializationContext;
-import com.fasterxml.jackson.dataformat.yaml.YAMLFactory;
-import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
-
-import ch.ocsource.yasr.jackson.BeanDeserializerFactory;
 import ch.ocsource.yasr.parser.ConverterBase;
 import ch.ocsource.yasr.parser.version1_0_0.models.Document;
 
